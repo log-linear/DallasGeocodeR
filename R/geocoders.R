0e099eda1097ed383c64cc1e17f4259032491992
victor.faner <victor.faner@dallascityhall.com>
2021-05-28 13:14:47 -0500

    Add reverse_geocode function

--- R/geocoders.R
+++ R/geocoders.R
@@ -13,9 +13,10 @@
 #' @param city optional, character vector of city names.
 #' @param zip optional, character vector of postal codes.
 #' @param id optional, numeric vector uniquely identifying each address,
-#'          defaults to \code{seq(street)} if not provided. If providing this
-#'          argument, note that each integer in the vector must be unique.
-#' @return A \code{data.frame} of geocoded address results.
+#'   defaults to \code{seq(street)} if not provided. If providing this
+#'   argument, note that each integer in the vector must be unique.
+#' @return A \code{data.frame} of geocoded address results. Includes original
+#'   address alongside generated lat/long coordinates.
 #' @examples
 #' addresses <- data.frame(
 #'   street = c('8525 Garland Rd', '1500 Marilla St', '3809 Grand Avenue'),
@@ -101,6 +102,68 @@ geocode_addresses <- function(street, city = NULL, zip = NULL, id = NULL) {
   return(results)
 }
 
+#' Reverse Geocode addresses using the City of Dallas's public geocoding service
+#'
+#' Determine addresses from lat/long coordinates using the City of Dallas's
+#' public geocoding service. See the
+#' \href{https://gis.dallascityhall.com/wwwgis/rest/services/ToolServices/
+#' DallasStreetsLocator/GeocodeServer}{ArcGIS REST API endpoint} for additional
+#' details.
+#'
+#' @param latitude scalar, latitude coordinate to reverse geocode,
+#' @param longitude scalar, longitude coordinate to reverse geocode
+#' @param intersection logical boolean value specifying whether the
+#'   geocode service should return the nearest street intersection or the
+#'   nearest address to the given point. Default is FALSE.
+#' @param distance scalar, maximum distance in meters from coordinates
+#'   within which a matching address should be searched. Default value is 0.
+#' @return A single row \code{data.frame} of the reverse-geocoded address with
+#'   columns /code{street}, /code{city}, and /code{zip}
+reverse_geocode <- function(latitude, longitude, intersection = F,
+                            distance = 0) {
+
+  geocoder_url <- paste0(
+    "https://gis.dallascityhall.com/wwwgis/rest/services",
+    "/ToolServices/DallasStreetsLocator/GeocodeServer/reverseGeocode"
+  )
+
+  # Input validation
+  if (any(length(latitude) > 1, length(longitude) > 1, length(distance) > 1,
+          length(intersection) > 1)) {
+    stop("All arguments must be of length 1.")
+  }
+
+  latitude <- as.numeric(latitude)
+  longitude <- as.numeric(longitude)
+  distance <- ifelse(is.null(distance) | distance < 1,
+                     "",
+                     paste0("&distance=", distance))
+  intersection <- ifelse(intersection == T, "true", "false")
+
+  # Format Request URL
+  req_url <- paste0(geocoder_url,
+                    "?location=", latitude, ",", longitude,
+                    distance,
+                    "&returnIntersection=", intersection,
+                    "&f=json")
+
+  # Submit request
+  request <- httr::POST(url = req_url)
+
+  # Extract content
+  response <- httr::content(request, "parsed", "application/json")
+
+  # Final result
+  result <- data.frame(
+    street = response$address$Street,
+    city = response$address$City,
+    zip = response$address$ZIP,
+    latitude = latitude,
+    longitude = longitude
+  )
+  return(result)
+}
+
 #===============================================================================
 # Test code
 #===============================================================================
@@ -112,3 +175,5 @@ addresses <- data.frame(
 
 test <- geocode_addresses(addresses$street, addresses$city, addresses$zip)
 test <- geocode_addresses(addresses$street)
+
+test2 <- reverse_geocode(test$latitude[[1]], test$longitude[[1]])
