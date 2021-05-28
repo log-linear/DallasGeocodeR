391e6879c38831a1f1fad8f4f0b8578817a4fd40
victor.faner <victor.faner@dallascityhall.com>
2021-05-28 11:23:46 -0500

    Add geocode_addresses documentation
    - Optimize lapply usage in geocode_addresses
    - New optional id argument for geocode_addresses

--- R/geocoders.R
+++ R/geocoders.R
@@ -1,29 +1,63 @@
 
-geocode_addresses <- function(street, city = NULL, zip = NULL) {
+#' Geocode addresses using the City of Dallas's public geocoding service
+#'
+#' Calculate lat/long coordinates for a given set of addresses using the City of
+#' Dallas's public geocoding service. Note that this service can geocode a
+#' \strong{maximum 1000 addresses} per request. Calling this function on more
+#' than 1000 addresses will return an error. See the
+#' \href{https://gis.dallascityhall.com/wwwgis/rest/services/ToolServices/
+#' DallasStreetsLocator/GeocodeServer}{ArcGIS REST API endpoint} for additional
+#' details.
+#'
+#' @param street character vector of street addresses.
+#' @param city optional, character vector of city names.
+#' @param zip optional, character vector of postal codes.
+#' @param id optional, numeric vector uniquely identifying each address,
+#'          defaults to \code{seq(street)} if not provided. If providing this
+#'          argument, note that each integer in the vector must be unique.
+#' @return A \code{data.frame} of geocoded address results.
+#' @examples
+#' addresses <- data.frame(
+#'   street = c('8525 Garland Rd', '1500 Marilla St', '3809 Grand Avenue'),
+#'   city = c('Dallas', 'Dallas', 'Dallas'),
+#'   zip = c('75218', '75201', '75210')
+#' )
+#'
+#' geocode_addresses(addresses$street, addresses$city, addresses$zip)
+#' geocode_addresses(addresses$street)
+geocode_addresses <- function(street, city = NULL, zip = NULL, id = NULL) {
 
-  batch_size <- 1000  # Hardcoded server limit
-  geocoder_url <- "https://gis.dallascityhall.com/wwwgis/rest/services/ToolServices/DallasStreetsLocator/GeocodeServer/geocodeAddresses"
-
+  batch_size <- 1000
+  geocoder_url <- paste0(
+    "https://gis.dallascityhall.com/wwwgis/rest/services",
+    "/ToolServices/DallasStreetsLocator/GeocodeServer/geocodeAddresses"
+  )
   n_addresses <- length(street)
+
+  # Check that total addresses <= server maximum
   if (n_addresses > batch_size) {
-    stop(paste("geocode_addresses can only process a maximum",
+    stop(paste("COD Geocoder can only process a maximum",
                batch_size,
                "addresses at once."))
   }
 
-  # Set city/zip to empty strings if not provided
-  if (is.null(city)) city <- rep("", n_addresses)
-  if (is.null(zip)) zip <- rep("", n_addresses)
-
-  # Handle any missing values
+  # Input validation
   street <- stringr::str_replace_na(street, "")
-  city <- stringr::str_replace_na(city, "")
-  zip <- stringr::str_replace_na(zip, "")
+
+  # Handle optional args if not provided
+  if (is.null(city)) city <- rep("", n_addresses)
+  else city <- stringr::str_replace_na(city, "")
+
+  if (is.null(zip)) zip <- rep("", n_addresses)
+  else zip <- stringr::str_replace_na(zip, "")
+
+  if (is.null(id)) id <- seq(street)
+  else id <- as.numeric(id)
 
   # Create JSON payload
   nested_list <- list(
-    records = lapply(seq(street),
-                     function(i) list(attributes = list(OBJECTID = i,
+    records = lapply(id,
+                     function(i) list(attributes = list(OBJECTID = id[[i]],
                                                         Street = street[[i]],
                                                         City = city[[i]],
                                                         ZIP = zip[[i]])))
@@ -50,9 +84,9 @@ geocode_addresses <- function(street, city = NULL, zip = NULL) {
   locations <- response$locations
 
   attributes <- lapply(
-    seq(locations),
+    id,
     function(i) list(
-      id = locations[[i]]$attributes$ResultID,
+      id = id[[i]],
       latitude = locations[[i]]$attributes$X,
       longitude = locations[[i]]$attributes$Y,
       score = locations[[i]]$score,
@@ -71,9 +105,9 @@ geocode_addresses <- function(street, city = NULL, zip = NULL) {
 # Test code
 #===============================================================================
 addresses <- data.frame(
-  street = c('5500 Reiger Ave', '1500 Marilla St', '9231 Pinewood Dr'),
+  street = c('8525 Garland Rd', '1500 Marilla St', '3809 Grand Avenue'),
   city = c('Dallas', 'Dallas', 'Dallas'),
-  zip = c('75214', '75201', '75243')
+  zip = c('75218', '75201', '75210')
 )
 
 test <- geocode_addresses(addresses$street, addresses$city, addresses$zip)
